90a20df6123113e1847716d3780e99d9d15d5658
GabrielGregerson <Spidy700@hotmail.com>
2025-04-25 13:33:52 -0500

    Add project files.

---
+++ SpaceFighter/BioEnemyShip.cpp
@@ -0,0 +1,36 @@
+
+#include "BioEnemyShip.h"
+#include "Level.h"
+
+
+BioEnemyShip::BioEnemyShip()
+{
+	SetSpeed(150);
+	SetMaxHitPoints(1);
+	SetCollisionRadius(20);
+}
+
+
+void BioEnemyShip::Update(const GameTime& gameTime)
+{
+	if (IsActive())
+	{
+		float x = sin(gameTime.GetTotalTime() * 6 * 5 +  GetIndex());
+		x *= GetSpeed() * gameTime.GetElapsedTime() * 5.4f;
+		TranslatePosition(x, GetSpeed() * gameTime.GetElapsedTime());
+
+		if (!IsOnScreen()) Deactivate();
+	}
+
+	EnemyShip::Update(gameTime);
+}
+
+
+void BioEnemyShip::Draw(SpriteBatch& spriteBatch)
+{
+	if (IsActive())
+	{
+		const float alpha = GetCurrentLevel()->GetAlpha();
+		spriteBatch.Draw(m_pTexture, GetPosition(), Color::WHITE * alpha, m_pTexture->GetCenter(), Vector2::ONE, Math::PI, 1);
+	}
+}
